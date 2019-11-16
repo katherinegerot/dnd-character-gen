0e4a8a82d62ba3e8a955a0b7587131f28c011ab6
Katherine Gerot <katie.gerot@huskers.unl.edu>
2019-11-16 14:36:04 -0600

    working on race profiles

--- basic_info.h
+++ basic_info.h
@@ -8,26 +8,44 @@
  * V1.0: Basic Races
  */
 
- enum race_basic {
-   Dragon_Born, //db
-   Dwarf, // dw
-   Elf, // el
-   Gnome, // gn
-   Half_Elf, //he
-   Halfling, //hf
-   Half_Orc, //ho
-   Tiefling //tf
- } Basic_Race;
+struct weapon {
+  char* damage_type;
 
+} Weapon;
 
- struct race_profile {
-   Basic_Race race;
-   char* name;
-   char* type;
-   int ability_modifiers;
-   int age_range[2];
-   double alignment_weight[6];
-   int height_avg;
-   int weight_avg;
-   int speed;
- } Race_Profile;
+enum race_basic {
+  Dragon_Born, //db
+  Dwarf, // dw
+  Elf, // el
+  Gnome, // gn
+  Half_Elf, //he
+  Halfling, //hf
+  Half_Orc, //ho
+  Tiefling //tf
+} Basic_Race;
+
+struct ability_scores{
+  int strength[2];
+  int dexterity[2];
+  int consitution[2];
+  int intelligence[2];
+  int wisdom[2];
+  int charisma[2];
+} Ability;
+
+// enum languages {
+//
+// } Language;
+
+struct race_profile {
+  Basic_Race race;
+  char* name;
+  char* type;
+  int ability_modifiers;
+  int age_range[2];
+  double alignment_weight[6];
+  int height_avg;
+  int weight_avg;
+  int speed;
+  Weapon* ancestry;
+} Race_Profile;
