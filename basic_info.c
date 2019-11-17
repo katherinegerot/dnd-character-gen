38dc3cd24dd659771e2e6140b22f5fd16d5c7172
Katherine Gerot <katie.gerot@huskers.unl.edu>
2019-11-16 19:13:04 -0600

    adding functions and simplifying version 1 contents

--- basic_info.c
+++ basic_info.c
@@ -1,11 +1,121 @@
+/**
+ * Author: Katherine Gerot
+ * Creation Date: 11/16/2019
+ * Last Edited: 11/16/2019
+ * Version 1.0
+ *
+ * Functions for character creation
+ * V1.0: Completing Dragon Born and Barbarian Class
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "basic_info.h"
+#include "names.h"
 
-Race_Profile race_profile_init(PHB_Race race_profile);
-Class_Profile class_profile_init(PHB_Class class_profile);
+Race_Profile race_profile_init(PHB_Race race){
+  Race_Profile rp;
+  rp->race = race;
+  rp->names = possible_names[race];
+  switch (race) {
+    case Dragon_Born:
+      rp->types = {"black", "blue", "brass", "bronze", "copper", "gold",
+                   "green", "red", "silver", "white"};
+      rp->resistances = {"the damage type associated with your draconic ancestry"};
+      rp->languages = {Common, Draconic};
+      rp->ability_modifiers = {2,0,0,0,0,1};
+      rp->age_range = {15,80};
+      rp->alignment_weight = {8,2,5,5,2,8};
+      rp->height_avg = 6.5;
+      rp->weight_avg = 250;
+      rp->speed = 30;
+      rp->ancestry = create_dragonborn_ancestry();
+      break;
+  }
+  return rp;
+}
+//enum Equipment_Type {Light_Armour,Medium_Armour,Heavy_Armour,Shield,Tool};
+//Weapon_Type {Simple,Martial,Breath};
+Class_Profile class_profile_init(PHB_Class class){
+  Class_Profile cp;
+  cp->class = class;
+  switch (class) {
+    case Barbarian:
+      cp->starting_equip = {{"Greataxe",""},{"Two Handaxes",""},{"Explorer's Pack"},{"Four Javelins"}};
+      cp->features;
+      cp->hit_points;//int
+      cp->hit_dice;//int
+      cp->ability_weights;//int* for rest
+      // cp->armour_proficiencies;
+      // cp->weapon_proficiences;
+      break;
+  }
+  return cp;
+}
+Ability_Scores gen_abilities(Class_Profile class){
 
-// Ability scores and Skills
-Ability_Scores gen_abilities(Class_Profile class);
-Skills gen_skills(Ability_Scores abilities);
+}
+Skills gen_skills(Ability_Scores abilities){
+
+}
+
+char ** create_dragonborn_ancestry(){
+  char ** a = (char**)malloc(sizeof(char*) * 10);
+  for(int i = 0; i < 10; i++) {
+    switch (i) {
+      case 0: case 4: a[i] = "Acid Breath, 5 by 30 ft. line (Dex. save)";
+        break;
+      case 1: case 3: a[i] = "Lightning Breath, 5 by 30 ft. line (Dex. save)";
+        break;
+      case 2: a[i] = "Fire Breath, 5 by 30 ft. line (Dex. save)";
+        break;
+      case 5: case 7: a[i] = "Fire Breath, 15 ft. cone (Dex. save)";
+        break;
+      case 8: case 9: a[i] = "Cold Breath, 15 ft. cone (Con. save)";
+        a[i] = create_weapon(Breath,Cold,2,8,"15 ft. cone","Can be used once every rest","2d6");
+        break;
+      case 6: a[i] = "Poison Breath, 15 ft. cone (Con. save)";
+        break;
+    }
+  }
+}
+
+// Weapon * create_dragonborn_ancestry(){
+//   Weapon * a = (Weapon*)malloc(sizeof(Weapon) * 10);
+//   for(int i = 0; i < 10; i++) {
+//     switch (i) {
+//       case 0: case 4:
+//         a[i] = create_weapon(Breath,Acid,1,8,"5 by 30 ft. line","Can be used once every rest","2d6");
+//         break;
+//       case 1: case 3:
+//         a[i] = create_weapon(Breath,Lightning,1,8,"5 by 30 ft. line","Can be used once every rest","2d6");
+//         break;
+//       case 2:
+//         a[i] = create_weapon(Breath,Fire,1,8,"5 by 30 ft. line","Can be used once every rest","2d6");
+//         break;
+//       case 5: case 7:
+//         a[i] = create_weapon(Breath,Fire,1,8,"15 ft. cone","Can be used once every rest","2d6");
+//         break;
+//       case 8: case 9:
+//         a[i] = create_weapon(Breath,Cold,2,8,"15 ft. cone","Can be used once every rest","2d6");
+//         break;
+//       case 6:
+//         a[i] = create_weapon(Breath,Poison,2,8,"15 ft. cone","Can be used once every rest","2d6");
+//         break;
+//     }
+//   }
+//   return a;
+// }
+// Weapon create_weapon(Weapon_Type w_type,Damage_Type d_type,char save,
+//   int throw_max,char* distance,char* cooldown, char* damage){
+//     Weapon weapon;
+//     weapon->weapon_type = w_type;
+//     weapon->damage_type = d_type;
+//     weapon->save = save;
+//     weapon->throw_max = throw_max;
+//     weapon->distance = distance;
+//     weapon->cooldown = cooldown;
+//     weapon->damage = damage;
+//     return weapon;
+// }
--- basic_info.h
+++ basic_info.h
@@ -9,45 +9,57 @@
  */
 
 // Profile Initializers
-Race_Profile race_profile_init(PHB_Race race_profile);
-Class_Profile class_profile_init(PHB_Class class_profile);
+Race_Profile race_profile_init(PHB_Race race);
+Class_Profile class_profile_init(PHB_Class class);
 
 // Ability scores and Skills
 Ability_Scores gen_abilities(Class_Profile class);
 Skills gen_skills(Ability_Scores abilities);
 
+// Item Creators
+// Weapon create_weapon(Weapon_Type w_type,Damage_Type d_type,int save,
+//   int throw_max,char* distance,char* cooldown, char* damage);
+char ** create_dragonborn_ancestry();
 
 // Enumerators for easier identification
 enum PHB_Race {Dragon_Born,Dwarf,Elf,Gnome,Half_Elf,Halfling,Half_Orc,
                Human,Tiefling};
 enum PHB_Class {Barbarian,Bard,Cleric,Druid,Fighter,Monk,Paladin,Ranger,
                 Rogue,Sorcerer,Warlock,Wizard};
-enum Gender {Male,Female,Agender,Non-binary,Bigender,Unknown};
+enum Gender {Male,Female,Agender,Non_Binary,Bigender,Unknown};
 enum Language {Common,Draconic};
 enum Equipment_Type {Light_Armour,Medium_Armour,Heavy_Armour,Shield,Tool};
-Weapon_Type {Simple,Martial};
+Weapon_Type {Simple,Martial,Breath};
 enum Damage_Type {Acid,Lightning,Fire,Poison,Cold};
 
 // Race-specific alterations
 struct Race_Profile {
   PHB_Race race;
-  char** names, types, resistances;
-  int ability_modifiers[6],age_range[2];
-  double alignment_weight[6];
-  int height_avg,weight_avg,speed;
-  Weapon* ancestry;
+  char*** names, types, resistances;
+  int ability_modifiers[6],age_range[2],alignment_weight[6];
+  Language* languages;
+  double height_avg;
+  int weight_avg,speed;
+  char** ancestry;
 };
 
 struct Class_Profile {
+  PHB_Class class;
   char** features;
   int hit_points,hit_dice;
-  double* ability_weights,armour_proficiencies,weapon_proficiences;
+  int* ability_weights;
+  int* armour_proficiencies,weapon_proficiences;
+  char*** starting_equip;
 };
 
 // Holds ability scores and modifiers
 struct Ability_Scores{
    int strength[2],dexterity[2],consitution[2],intelligence[2],wisdom[2],
        charisma[2];
+};
+struct Alignment {
+  int lawful_good,neutral_good,chaotic_good,lawful_neutral,true_neutral,
+      chaotic_neutral,lawful_evil,neutral_evil,chaotic_evil;
 };
 
 // Skill structure
@@ -58,26 +70,24 @@ struct Skills {
 };
 
 // Specific weapon and attack ability breakdown
-struct Weapon {
-  Weapon_Type weapon_type;
-  Damage_Type damage_type;
-  char save;
-  int throw_max;
-  double distance; //in feet
-  char* direction,cooldown,damage;
-};
+// struct Weapon {
+//   Weapon_Type weapon_type;
+//   Damage_Type damage_type;
+//   int save, throw_max;
+//   char* distance,cooldown,damage;
+// };
 
 // Player's money
 struct Purse {
   int ep,pp,gp,sp,cp;
 };
 
-struct Equipment {
-  Equipment_Type type;
-  char* name, decription;
-  int cost,weight;
-  int* likely_classes;
-};
+// struct Equipment {
+//   Equipment_Type type;
+//   char* name, decription;
+//   int cost,weight;
+//   int* likely_classes;
+// };
 
 // Character profile
 struct Character {
@@ -87,7 +97,7 @@ struct Character {
   Gender gender;
   char* name,type;
   Ability_Scores abilities;
-  Weapon* weapons;
+  char** weapons;
   Equipment* equipment;
   Purse purse;
   Skills skills;
